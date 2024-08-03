5a8bc0a102c92a25bfe18ee820f70ea334da0830
amnaahad <amnakhan200427@gmail.com>
2024-08-03 17:12:20 +0500

    commiting changes

--- tw/src/components/Places/Places.jsx
+++ tw/src/components/Places/Places.jsx
@@ -1,124 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PlaceCard from './PlaceCard';
 import Modal from '../Modal/Modal';
-import Img1 from '../../assets/places/boat.jpg';
-import Img2 from '../../assets/places/tajmahal.jpg';
-import Img3 from '../../assets/places/water.jpg';
-import Img4 from '../../assets/places/place4.jpg';
-import Img5 from '../../assets/places/place5.jpg';
-import Img6 from '../../assets/places/place6.jpg';
-import Img7 from '../../assets/places/bashu.png';
-import Img8 from '../../assets/places/neelam.png';
-import Img9 from '../../assets/places/sharan.png';
-import Img10 from '../../assets/places/naltar.png';
 import { Link } from 'react-router-dom';
 
-const PlacesData = [
-  {
-    id: 1,
-    img: Img1,
-    title: 'Skardu',
-    location: 'Pakistan',
-    description: 'Skardu, nestled in the heart of the Gilgit-Baltistan region, is renowned for its breathtaking landscapes, including towering peaks, serene lakes, and stunning valleys, making it a paradise for adventure enthusiasts and nature lovers.',
-    price: 36000,
-    days: 3,
-    month: 'June',
-  },
-  {
-    id: 2,
-    img: Img2,
-    title: 'Fairy Meadows',
-    location: 'Pakistan',
-    description: 'Fairy Meadows is a stunningly beautiful alpine meadow located at the base of Nanga Parbat in northern Pakistan, offering breathtaking views and serene landscapes.',
-    price: 27000,
-    days: 4,
-    month: 'July',
-  },
-  {
-    id: 3,
-    img: Img3,
-    title: 'Naran Kaghan',
-    location: 'Pakistan',
-    description: 'Naran Kaghan is a breathtaking valley in northern Pakistan renowned for its scenic landscapes, lush green meadows, and pristine lakes, offering a serene escape into nature\'s beauty.',
-    price: 16000,
-    days: 5,
-    month: 'August',
-  },
-  {
-    id: 4,
-    img: Img4,
-    title: 'Hunza',
-    location: 'Pakistan',
-    description: 'Hunza is a stunning valley in northern Pakistan renowned for its breathtaking landscapes, towering mountains, and vibrant culture.',
-    price: 32000,
-    days: 6,
-    month: 'September',
-  },
-  {
-    id: 5,
-    img: Img5,
-    title: 'Kumrat',
-    location: 'Pakistan',
-    description: 'Kumrat is a picturesque valley in Pakistan\'s Khyber Pakhtunkhwa province, renowned for its lush landscapes, crystal-clear rivers, and stunning mountain views.',
-    price: 23000,
-    days: 3,
-    month: 'October',
-  },
-  {
-    id: 6,
-    img: Img6,
-    title: 'Kashmir',
-    location: 'Pakistan',
-    description: 'Kashmir, often referred to as Paradise on Earth is a picturesque region in South Asia known for its stunning landscapes, including lush valleys, snow-capped mountains, and serene lakes.',
-    price: 15200,
-    days: 4,
-    month: 'November',
-  },
-  {
-    id: 7,
-    img: Img7,
-    title: 'Bashu',
-    location: 'Pakistan',
-    description: 'Bashu Valley is a picturesque region in Pakistan\'s Balochistan province, renowned for its stunning natural landscapes, lush greenery, and serene environment.',
-    price: 20000,
-    days: 5,
-    month: 'December',
-  },
-  {
-    id: 8,
-    img: Img8,
-    title: 'Neelam Valley',
-    location: 'Pakistan',
-    description: 'Neelam Valley, located in the Azad Kashmir region of Pakistan, is renowned for its breathtaking landscapes, lush green meadows, and crystal-clear rivers.',
-    price: 13200,
-    days: 6,
-    month: 'January',
-  },
-  {
-    id: 9,
-    img: Img9,
-    title: 'Sharan Forest',
-    location: 'Pakistan',
-    description: 'Sharan Forest is a serene and lush green woodland located in the Kaghan Valley of Pakistan, known for its dense coniferous trees and tranquil natural beauty.',
-    price: 15200,
-    days: 3,
-    month: 'February',
-  },
-  {
-    id: 10,
-    img: Img10,
-    title: 'Naltar Valley',
-    location: 'Pakistan',
-    description: 'Naltar Valley, located in the Gilgit-Baltistan region of Pakistan, is renowned for its stunning landscapes, vibrant lakes, and skiing opportunities.',
-    price: 15200,
-    days: 4,
-    month: 'March',
-  },
-];
-
 const Places = ({ limit, heading }) => {
+  const [placesData, setPlacesData] = useState([]);
   const [selectedPlace, setSelectedPlace] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    // Fetch data from JSON server
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/places');
+        setPlacesData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleOpenModal = (place) => {
     setSelectedPlace(place);
@@ -130,7 +32,7 @@ const Places = ({ limit, heading }) => {
     setSelectedPlace(null);
   };
 
-  const displayedPlaces = limit ? PlacesData.slice(0, limit) : PlacesData;
+  const displayedPlaces = limit ? placesData.slice(0, limit) : placesData;
 
   return (
     <div className="bg-gray-50 py-10">
